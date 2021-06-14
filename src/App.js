e53dd27266c414c932f871792dffd89125bcf384
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2021-06-14 20:09:38 +0100

    Projeto comentado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+// ***********************************************
+// app.js
+// ***********************************************
+
+
+
 import logo from './logo.svg';
 import './App.css';
 
+
+
+/**
+ * isto é um COMPONENTE
+ * neste caso particular, é designado 'componente simples' pq é uma função
+ * os componentes são formas de distribuir o código por diferentes 'objetos'
+ * algo que conhecemos muito bem de POO
+ * @returns 
+ */
 function App() {
+  // se o componente for do tipo FUNÇÃO obrigatoriamente tem um RETURN
   return (
+    // só devolve UM elemento
     <div className="App">
+      {/* no JSX não se pode usar 'class' para referenciar uma classe de CSS
+          pois esse nome é reservado
+          Por isso, temos de usar 'className', e depois o React faz a transformação */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* o acesso a variáveis, objetos, etc é SEMPRE feito entre {}
+            Equivalente ao que fazemos em Razor, com @NomeVar */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -22,4 +44,5 @@ function App() {
   );
 }
 
+// um componente só é acessível se for EXPORTADO
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
+// ************************************************
+// index.js
+// ************************************************
+
+// adição de recursos necessários ao funcionamento da app
+// ********************************************************
+// criar o objeto React (cor preta) que extende de 'react'
 import React from 'react';
+// criar o objeto ReactDOM (cor preta) que extende de 'react-dom'
 import ReactDOM from 'react-dom';
+// incluir o ficheiro css
 import './index.css';
+// criar o objeto App (cor preta) que extende de './App'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// toda a app React precisa de 'renderizar' um conteúdo para o browser
 ReactDOM.render(
+  // aqui, o código da linha 8 só é preciso por causa desta instrução seguinte
+  // a opção StrictMode só funciona em modo Developer
+  // mostra os erros detalhados no ecrã
   <React.StrictMode>
-    <App />
+    {/* daqui para cima estávamos a escrever javaScript.
+      Por isso, as linhas de comentário são  iniciadas por // ou por /*
+      Mas, aqui, estamos a escrever em JSX - JavaScript and XML
+      Logo, as linhas de comentário são incluídas em {} com os símbolos de /*
+  */}
+    <App /> {/* <----   código a ser executado e enviado para o Browser
+                        tecnicamente, chamar-lhe-emos de 'componente'
+                         */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') // <--- onde o Render irá atuar
 );
 
 // If you want to start measuring performance in your app, pass a function
